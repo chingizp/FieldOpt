7bf6653cb99e6d4e862810835f9d6ff629c1a9ad
chingizp <chingiz.panahli@gmail.com>
2017-07-17 22:20:59 +0200

    Added constructor for PSO class and override virtual functions

--- FieldOpt/Optimization/optimizers/PSO.cpp
+++ FieldOpt/Optimization/optimizers/PSO.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "PSO.h"
+
+namespace Optimization{
+    namespace Optimizers{
+
+        PSO::PSO(Settings::Optimizer *settings, Case *base_case,
+                 Model::Properties::VariablePropertyContainer *variables, Reservoir::Grid::Grid *grid, Logger *logger)
+                : Optimizer(settings, base_case, variables, grid, logger) {
+
+        }
+
+        void PSO::iterate() {
+
+        }
+
+        Optimizer::TerminationCondition PSO::IsFinished() {
+            return MINIMUM_STEP_LENGTH_REACHED;
+        }
+
+        void PSO::handleEvaluatedCase(Case *c) {
+
+        }
+    }
+}
--- FieldOpt/Optimization/optimizers/PSO.h
+++ FieldOpt/Optimization/optimizers/PSO.h
@@ -5,10 +5,26 @@
 #ifndef FIELDOPT_PSO_H
 #define FIELDOPT_PSO_H
 
+#include "optimizer.h"
+#include "Particle.h"
 
-class PSO {
+namespace Optimization{
+    namespace Optimizers{
+        class PSO : public Optimizer{
+        public:
+            PSO(Settings::Optimizer *settings, Case *base_case, Model::Properties::VariablePropertyContainer *variables,
+            Reservoir::Grid::Grid *grid, Logger *logger);
 
-};
+        private:
+            void iterate() override ;
+            TerminationCondition IsFinished() override ;
+        protected:
+            void handleEvaluatedCase(Case *c) override ;
+
+        };
+    }
+}
+
 
 
 #endif //FIELDOPT_PSO_H
