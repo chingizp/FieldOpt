fd69e82d6bbad2c5568967026714f88afbbfc437
chingizp <chingiz.panahli@gmail.com>
2017-07-17 17:13:10 +0200

    Added PSO class

---
+++ FieldOpt/Optimization/optimizers/PSO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chingizp on 7/17/17.
+//
+
+#include "PSO.h"
---
+++ FieldOpt/Optimization/optimizers/PSO.h
@@ -0,0 +1,14 @@
+//
+// Created by chingizp on 7/17/17.
+//
+
+#ifndef FIELDOPT_PSO_H
+#define FIELDOPT_PSO_H
+
+
+class PSO {
+
+};
+
+
+#endif //FIELDOPT_PSO_H
