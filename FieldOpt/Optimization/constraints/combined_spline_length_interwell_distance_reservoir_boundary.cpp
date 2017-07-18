55f01777c0383bc555221b0c407f37817bb3c663
chingizp <chingiz.panahli@gmail.com>
2017-07-18 19:30:38 +0200

    Added functions to get the coordinate ids, well length, interwell distance according to the well names and case.

--- FieldOpt/Optimization/constraints/combined_spline_length_interwell_distance_reservoir_boundary.cpp
+++ FieldOpt/Optimization/constraints/combined_spline_length_interwell_distance_reservoir_boundary.cpp
@@ -10,11 +10,13 @@ namespace Optimization {
                 Model::Properties::VariablePropertyContainer *variables,
                 Reservoir::Grid::Grid *grid)
         {
+            min_length_=settings.min_length;
+            max_length_=settings.max_length;
             max_iterations_ = settings.max_iterations;
             Settings::Optimizer::Constraint dist_constr_settings;
             dist_constr_settings.wells = settings.wells;
             dist_constr_settings.min = settings.min_distance;
-            distance_constraint_ = new InterwellDistance(dist_constr_settings, variables);
+            //distance_constraint_ = new InterwellDistance(dist_constr_settings, variables);
             Settings::Settings global_settings_;
 
             length_constraints_ = QList<WellSplineLength *>();
@@ -24,6 +26,7 @@ namespace Optimization {
                 len_constr_settings.min = settings.min_length;
                 len_constr_settings.max = settings.max_length;
                 length_constraints_.append(new WellSplineLength(len_constr_settings, variables));
+                affected_wells_.append(initializeWell(variables->GetWellSplineVariables(wname)));
 
                 Settings::Optimizer::Constraint boundary_constraint_settings;
                 boundary_constraint_settings.box_imin = settings.box_imin;
@@ -34,6 +37,9 @@ namespace Optimization {
                 boundary_constraint_settings.box_kmax = settings.box_kmax;
                 boundary_constraint_settings.well = wname;
                 boundary_constraints_.append(new ReservoirBoundary(boundary_constraint_settings, variables, grid));
+            }
+            for (int i = 0; i < settings.wells.size(); ++i) {
+                affected_wells_[i].name=settings.wells[i];
             }
         }
 
@@ -90,6 +96,121 @@ namespace Optimization {
         }
         return ubounds;
     }
+
+        double CombinedSplineLengthInterwellDistanceReservoirBoundary::get_well_min_length() {
+            return min_length_;
+        }
+
+        double CombinedSplineLengthInterwellDistanceReservoirBoundary::get_well_max_length() {
+            return max_length_;
+        }
+
+        double CombinedSplineLengthInterwellDistanceReservoirBoundary::get_well_length(Case *c, QString name) {
+            double heel_x_val,heel_y_val,heel_z_val,toe_x_val,toe_y_val,toe_z_val,d;
+            Eigen::Vector3d heel_vals, toe_vals, heel_to_toe_vec;
+            for (Well well:affected_wells_) {
+                if (QString::compare(well.name, name)==0) {
+                    heel_x_val = c->real_variables()[well.heel.x];
+                    heel_y_val = c->real_variables()[well.heel.y];
+                    heel_z_val = c->real_variables()[well.heel.z];
+
+                    toe_x_val = c->real_variables()[well.toe.x];
+                    toe_y_val = c->real_variables()[well.toe.y];
+                    toe_z_val = c->real_variables()[well.toe.z];
+
+                    heel_vals << heel_x_val, heel_y_val, heel_z_val;
+                    toe_vals << toe_x_val, toe_y_val, toe_z_val;
+                    heel_to_toe_vec = toe_vals - heel_vals;
+                    d = heel_to_toe_vec.norm();
+                }
+            }
+            return d;
+        }
+
+        double CombinedSplineLengthInterwellDistanceReservoirBoundary::get_shortest_distance_2_wells(Case *c, QString name1, QString name2) {
+            QList<Eigen::Vector3d> coords;
+            double shortest_distance;
+            for (Well well : affected_wells_) {
+                if ((QString::compare(well.name,name1)==0) || (QString::compare(well.name,name2)==0)) {
+                    double heel_x_val = c->real_variables()[well.heel.x];
+                    double heel_y_val = c->real_variables()[well.heel.y];
+                    double heel_z_val = c->real_variables()[well.heel.z];
+
+                    double toe_x_val = c->real_variables()[well.toe.x];
+                    double toe_y_val = c->real_variables()[well.toe.y];
+                    double toe_z_val = c->real_variables()[well.toe.z];
+
+                    Eigen::Vector3d heel_vals;
+                    Eigen::Vector3d toe_vals;
+                    heel_vals << heel_x_val, heel_y_val, heel_z_val;
+                    toe_vals << toe_x_val, toe_y_val, toe_z_val;
+                    coords.append(heel_vals);
+                    coords.append(toe_vals);
+                }
+            }
+            shortest_distance=WellConstraintProjections::shortest_distance(coords);
+            return shortest_distance;
+        }
+
+        QUuid CombinedSplineLengthInterwellDistanceReservoirBoundary::get_heel_x_id(QString name) {
+            QUuid heelx;
+            for (Well well:affected_wells_) {
+                if (QString::compare(well.name, name)==0) {
+                    heelx=well.heel.x;
+                }
+            }
+            return heelx;
+        }
+
+        QUuid CombinedSplineLengthInterwellDistanceReservoirBoundary::get_heel_y_id(QString name) {
+            QUuid heely;
+            for (Well well:affected_wells_) {
+                if (QString::compare(well.name, name)==0) {
+                    heely=well.heel.y;
+                }
+            }
+            return heely;
+        }
+
+        QUuid CombinedSplineLengthInterwellDistanceReservoirBoundary::get_heel_z_id(QString name) {
+            QUuid heelz;
+            for (Well well:affected_wells_) {
+                if (QString::compare(well.name, name)==0) {
+                    heelz=well.heel.z;
+                }
+            }
+            return heelz;
+        }
+
+        QUuid CombinedSplineLengthInterwellDistanceReservoirBoundary::get_toe_x_id(QString name) {
+            QUuid toex;
+            for (Well well:affected_wells_) {
+                if (QString::compare(well.name, name)==0) {
+                    toex=well.toe.x;
+                }
+            }
+            return toex;
+        }
+
+        QUuid CombinedSplineLengthInterwellDistanceReservoirBoundary::get_toe_y_id(QString name) {
+            QUuid toey;
+            for (Well well:affected_wells_) {
+                if (QString::compare(well.name, name)==0) {
+                    toey=well.toe.y;
+                }
+            }
+            return toey;
+        }
+
+        QUuid CombinedSplineLengthInterwellDistanceReservoirBoundary::get_toe_z_id(QString name) {
+            QUuid toez;
+            for (Well well:affected_wells_) {
+                if (QString::compare(well.name, name)==0) {
+                    toez=well.toe.z;
+                }
+            }
+            return toez;
+        }
 
     }
 }
--- FieldOpt/Optimization/constraints/combined_spline_length_interwell_distance_reservoir_boundary.h
+++ FieldOpt/Optimization/constraints/combined_spline_length_interwell_distance_reservoir_boundary.h
@@ -5,6 +5,7 @@
 #include "well_spline_length.h"
 #include "interwell_distance.h"
 #include "reservoir_boundary.h"
+#include "ConstraintMath/well_constraint_projections/well_constraint_projections.h"
 
 namespace Optimization { namespace Constraints {
 /*!
@@ -17,7 +18,7 @@ namespace Optimization { namespace Constraints {
  * and _one_ reservoir boundary constraint.
  */
 
-class CombinedSplineLengthInterwellDistanceReservoirBoundary : public Constraint
+class CombinedSplineLengthInterwellDistanceReservoirBoundary : public Constraint, WellSplineConstraint
 {
  public:
   CombinedSplineLengthInterwellDistanceReservoirBoundary(
@@ -31,12 +32,34 @@ class CombinedSplineLengthInterwellDistanceReservoirBoundary : public Constraint
   string name() override { return "CombinedSplineLengthInterwellDistanceReservoirBoundary"; }
 
   // Constraint interface
- public:
+    double get_well_min_length();
+
+    double get_well_max_length();
+
+    double get_well_length(Case *pCase, QString name);
+
+    double get_shortest_distance_2_wells(Case *pCase, QString name1, QString name2);
+
+    QUuid get_heel_x_id(QString name);
+
+    QUuid get_heel_y_id(QString name);
+
+    QUuid get_heel_z_id(QString name);
+
+    QUuid get_toe_x_id(QString name);
+
+    QUuid get_toe_y_id(QString name);
+
+    QUuid get_toe_z_id(QString name);
+
+public:
   bool CaseSatisfiesConstraint(Case *c);
   void SnapCaseToConstraints(Case *c);
 
  private:
   int max_iterations_;
+  double min_length_,max_length_;
+  QList<Well> affected_wells_;
   QList<WellSplineLength *> length_constraints_;
   QList<ReservoirBoundary *> boundary_constraints_;
   InterwellDistance *distance_constraint_;
