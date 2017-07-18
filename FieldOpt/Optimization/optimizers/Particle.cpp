7299551bae1b26e93cb37e4786c82fd9765f1df2
chingizp <chingiz.panahli@gmail.com>
2017-07-18 19:26:26 +0200

    Finished coding Particle class

--- FieldOpt/Optimization/optimizers/Particle.cpp
+++ FieldOpt/Optimization/optimizers/Particle.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "Particle.h"
+namespace Optimization{
+    namespace Optimizers{
+
+        Particle::Particle(Case *c, const QHash<QUuid, double> &velocity) {
+            particle_case_=c;
+            particle_velocity_=velocity;
+            velocity_id_index_map_=particle_velocity_.keys();
+        }
+    }
+}
--- FieldOpt/Optimization/optimizers/Particle.h
+++ FieldOpt/Optimization/optimizers/Particle.h
@@ -4,11 +4,35 @@
 
 #ifndef FIELDOPT_PARTICLE_H
 #define FIELDOPT_PARTICLE_H
+#include "Optimization/case.h"
 
 
-class Particle {
+namespace Optimization{
+    namespace Optimizers{
 
-};
+        /*!
+         * @brief The Particle class holds the case and associated velocitiy.
+         * The purpose of the Partilce class is to assign a unique velocity for each case.
+        */
+
+        class Particle {
+        public:
+            /*!
+            * @brief The Particle constructor receives the case and velocity and assigns them to a Particle object.
+           */
+            Particle(Case* c, const  QHash<QUuid , double> &velocity);
+            Case* get_case() const { return particle_case_; } //! Get case from particle object
+            QHash<QUuid,double > get_particle_velocity() const {return particle_velocity_;} //!< Get particle velocity
+            void set_particle_velocity(const QUuid id, const double val){particle_velocity_[id]=val;} //! Set particle velocity
+        private:
+            QHash<QUuid , double> particle_velocity_;
+            QList<QUuid > velocity_id_index_map_;
+            Case *particle_case_;
+        };
+    }
+}
 
 
 #endif //FIELDOPT_PARTICLE_H
+
+
